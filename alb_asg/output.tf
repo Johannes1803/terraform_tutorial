4c2a131cbdf395cfe67395f1abb4d93a6d88defc
johannes <johannes.birk11@web.de>
2024-06-16 11:54:31 +0200

    chore: fmt files

--- alb_asg/output.tf
+++ alb_asg/output.tf
@@ -1,4 +1,4 @@
 output "alb_dns_name" {
-    value = aws_lb.example.dns_name
-    description = "The domain name of the LB"
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the LB"
 }
